f87f235f8f01f45e44e80c507dc875faa19cc8e1
alynek <alyne0x67@gmail.com>
2024-04-15 11:40:50 -0300

    Fix name file and show mouse  color in grid

--- rainbowClick.js
+++ rainbowClick.js
@@ -1,8 +1,22 @@
 function setup() {
     createCanvas(600, 600);
+    colorMode(HSB)
 }
 
 function draw(){
+    background(0)
+
+    for(let x = 0; x < 30; x++){
+        for(let y = 0; y < 30; y++){
+
+            let distanceX = 30 * x
+            let distanceY = 30 * y
+            let distanceMouse = dist(distanceX, distanceY, mouseX, mouseY)
+            
+            fill(distanceMouse)
+            rect(distanceX, distanceY, 25, 25)
+        }
+    }
 }
 
 
